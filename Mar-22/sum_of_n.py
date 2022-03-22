0e38c42fcbf69aaf48f5b352469efee56219dd6f
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-03-21 23:41:19 -0500

    Create sum_of_n.py

---
+++ Mar-22/sum_of_n.py
@@ -0,0 +1,17 @@
+import time
+
+def sum_of_n(n):
+    start = time.time()
+    
+    #sum
+    
+    #end time
+    
+    #return sum, end-start
+    
+def main():
+    print(sum_of_n(100000))
+    print(sum_of_n(1000000))
+    print(sum_of_n(5000000))
+    
+main()
