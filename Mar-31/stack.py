76220da03da44ed19892fbe0359433c8724a9dcd
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-03-31 14:33:07 -0500

    Create stack.py

---
+++ Mar-31/stack.py
@@ -0,0 +1,23 @@
+# Create an implementation of stacks 
+
+class Stack:
+    __init__():
+        self._items = []
+        
+def main():
+    stack1 = Stack()
+    # stack1.push(12)
+    # stack1.push(4)
+    # print(stack1.peek())
+    
+    # val1 = stack1.pop()
+    # print(val1)
+    # print(stack1.is_empty()) #expecting False
+    # print(stack1.size()) #expecting 1
+    
+    # val2 = stack1.pop()
+    # print(val2)
+    # print(stack1.is_empty()) #expecting True
+    # print(stack1.size()) #expecting 0
+    
+    
