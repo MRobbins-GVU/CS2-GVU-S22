eaa4f0299443cdb0db380b0c7d3376402c784f7e
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-04-19 19:34:22 -0500

    Sort files from class

---
+++ Apr-19/bubble_sort_optimized.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 18:32:24 2022
+
+@author: mrobbins
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 17:32:57 2022
+
+@author: mrobbins
+"""
+
+def bubble_sort(items): 
+    # Run through all the entries in items
+    for i in range(len(items)):
+        swapped = False
+        for j in range(len(items) - 1 - i):
+            # Compare the first item to the next 
+            if(items[j] > items[j+1]):
+                swapped = True
+                # Swap if needed 
+                temp = items[j+1] 
+                items[j+1] = items[j]
+                items[j] = temp
+                
+            print(i, j, items)
+    
+        if swapped == False:
+            print("already sorted!")
+            return items
+        
+    # Don't compare elements that you know are sorted
+    # ^^^ This step is accomplished by subtract i from
+    #     the range in j
+    
+    return items
+    
+def main():
+    # list1 = [ 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ] 
+    # print(list1)
+    
+    # sorted_list = bubble_sort(list1)
+    # print(sorted_list)
+    
+    
+    # list2 = [ 0, 1, 2, 3, 4 ] 
+    # print(list2)
+    
+    # sorted_list_2 = bubble_sort(list2)
+    # print(sorted_list_2)
+    
+    list3 = [ 0, 4, 1, 2, 3, 5, 6, 7, 8, 9 ] 
+    print(list3)
+    
+    sorted_list_3 = bubble_sort(list3)
+    print(sorted_list_3)
+    
+    
+main()
+
+
+
+
---
+++ Apr-19/selection_sort.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 18:44:30 2022
+
+@author: mrobbins
+"""
+
+def selection_sort(items):
+    # Go through every entry in items 
+    for i in range(len(items)):
+        # Assume the first is the largest then compare
+        largest_idx = 0
+        
+        # Find the largest
+        for j in range(len(items) - i):
+            if items[largest_idx] < items[j]:
+                largest_idx = j
+                   
+        
+        ### Swap the largest with the last unsorted item ###
+        # Save what is at the largest index
+        temp = items[largest_idx]
+        
+        # The number of sorted items is i
+        # Put the last unsorted item into where the largest was
+        items[largest_idx] = items[len(items) - i - 1]
+    
+        # Put the largest item into the last unsorted position
+        items[len(items) - i - 1] = temp
+        
+        print(i, items)
+    
+    return items
+        
+        
+    
+def main():
+    print("\n---List 1 ---")
+    list1 = [-1, -2, -3, -4]
+    print(list1)
+    sorted_list1 = selection_sort(list1)
+    print(sorted_list1)
+    
+    print("\n---List 2 ---")
+    list2 = [9, 9, 9, 9, 9, 9]
+    print(list2)
+    sorted_list2 = selection_sort(list2)
+    print(sorted_list2)
+       
+    print("\n---List 3 ---")
+    list3 = [1, 2, 3, 4, 5]
+    print(list3)
+    sorted_list3 = selection_sort(list3)
+    print(sorted_list3) 
+    
+    print("\n---List 4 ---")
+    list4 = [ 14, 2, 3, 11, 5]
+    print(list4)
+    sorted_list4 = selection_sort(list4)
+    print(sorted_list4) 
+    
+    
+    
+main()
+
+
+
